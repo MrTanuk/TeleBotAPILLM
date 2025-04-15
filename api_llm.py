314c947329dac2fafbd8950848accc6742e97bf6
MrTanuk <louismario.rr@gmail.com>
2025-04-15 01:01:20 -0400

    Improve historial and can chat better on private

--- api_llm.py
+++ api_llm.py
@@ -1,17 +1,16 @@
 import requests
 
-def get_api_llm(input_user, API_KEY, API_URL, LLM_MODEL):
+def get_api_llm(messages, API_TOKEN, API_URL, LLM_MODEL):
 
     # Define the headers for the API request
     headers = {
-    'Authorization': f'Bearer {API_KEY}',
-    'Content-Type': 'application/json'
+    'Authorization': f'Bearer {API_TOKEN}', 'Content-Type': 'application/json'
     }
 
     # Define the request payload (data)
     data = {
     "model": LLM_MODEL,
-    "messages": [{"role": "user", "content": input_user}]
+    "messages": messages
     }
 
     try:
