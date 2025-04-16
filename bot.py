6062a30b1ce4c229bc906aa3bc000ec0ef0e7d14
Luis Ramírez <102979749+MrTanuk@users.noreply.github.com>
2025-04-16 13:53:07 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -123,10 +123,10 @@ def webhook():
 
 # ========== Entry point =========
 if __name__ == '__main__':
-    if os.environ.get('RENDER'):
+    if os.environ.get('HOSTING'):
         from waitress import serve
         bot.remove_webhook()
         bot.set_webhook(url=WEBHOOK_URL + '/webhook')
-        serve(app, host='0.0.0.0', port=8080)  # Using waitress on a hosting (Render, for example)
+        serve(app, host='0.0.0.0', port=8080)  # Using waitress on a hosting
     else:
         app.run(host='0.0.0.0', port=8080, debug=False)
